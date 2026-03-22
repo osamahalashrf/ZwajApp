f46674c0b79f3f05fc6fca27796af2b75ff0b3c3
osamahalashrf <osamahalashrf@gmail.com>
2026-03-22 14:00:46 +0300

    initial Commit

---
+++ ZwajApp.API/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace ZwajApp.API.Models
+{
+    public class Value
+    {
+        public int id { get; set; }
+
+        public string Name { get ; set; }
+    }
+}
---
+++ ZwajApp.API/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using ZwajApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ZwajApp.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // we user sqlLite for development and sql server for production
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // add cors to allow cross origin requests from our angular app to our api
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // app.UseHsts();
+            }
+
+            // here we allow any origin to access our api, we can specify the allowed origins instead of allowing any origin
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            // app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
